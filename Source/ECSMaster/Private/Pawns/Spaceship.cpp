114b16e991bf728177c097ba04894d15e277c86d
fuegoglol <llou@etu.uqac.ca>
2025-01-21 17:44:18 -0500

    feat : Replicated movement with dedicated server

---
+++ Source/ECSMaster/Private/Pawns/Spaceship.cpp
@@ -0,0 +1,67 @@
+﻿// Lori LOU MIT License
+
+
+#include "ECSMaster/Public/Pawns/Spaceship.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Net/UnrealNetwork.h"
+
+
+// Sets default values
+ASpaceship::ASpaceship()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	Cabin = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Cabin"));
+	Cabin->SetupAttachment(RootComponent);
+
+	bReplicates = true;
+}
+
+void ASpaceship::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	DOREPLIFETIME(ASpaceship,SpaceshipLocation);
+}
+
+// Called when the game starts or when spawned
+void ASpaceship::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SpaceshipLocation = GetActorLocation();
+}
+
+void ASpaceship::Server_Move_Implementation(FVector Direction)
+{
+#if UE_SERVER || UE_EDITOR
+	SpaceshipLocation += Direction;
+#if UE_EDITOR
+	SetActorLocation(SpaceshipLocation);
+#endif
+	
+#endif
+}
+void ASpaceship::InputMove(FVector2D InputValue)
+{
+#if !UE_SERVER || UE_EDITOR
+	const FVector XDir = GetActorForwardVector()*InputValue.X;
+	const FVector YDir = GetActorRightVector()*InputValue.Y;
+	const FVector DesiredDir = XDir+YDir;
+	Server_Move(DesiredDir);
+#endif
+}
+
+void ASpaceship::OnRep_SpaceshipLocation(FVector_NetQuantize OldLocation)
+{
+	SetActorLocation(SpaceshipLocation);
+}
+
+// Called to bind functionality to input
+void ASpaceship::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+
---
+++ Source/ECSMaster/Public/Pawns/Spaceship.h
@@ -0,0 +1,47 @@
+﻿// Lori LOU MIT License
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Spaceship.generated.h"
+
+UCLASS()
+class ECSMASTER_API ASpaceship : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ASpaceship();
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Replicates properties
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable,Server,Reliable)
+	void Server_Move(FVector Direction);
+	
+	UFUNCTION(BlueprintCallable)
+	void InputMove(FVector2D InputValue);
+	
+private:
+
+	UFUNCTION()
+	void OnRep_SpaceshipLocation(FVector_NetQuantize OldLocation);
+
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly)
+	TObjectPtr<UStaticMeshComponent> Cabin;
+
+	UPROPERTY(ReplicatedUsing="OnRep_SpaceshipLocation")
+	FVector_NetQuantize SpaceshipLocation;
+};
